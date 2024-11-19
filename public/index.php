bb98b5b60f6a53490d0bc46edd62912d580f7306
farhanisty <farhannivtasija@gmail.com>
2024-11-20 01:52:06 +0700

    implements uri for get all menus

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Farhanisty\DonateloBackend\Repositories\MenuRepositoryImpl;
+use Farhanisty\Vetran\Facades\Response;
 use Farhanisty\Vetran\Application;
 
 require_once (__DIR__ . '/../vendor/autoload.php');
@@ -10,6 +12,27 @@ $dotenv->load();
 $app = new Application();
 $route = $app->getRoute();
 
-$route->get('api/menu', function () {
-    echo 'hello world';
+$route->get('api/menu', function () use ($app) {
+    $menuRepository = new MenuRepositoryImpl();
+
+    $menus = $menuRepository->getAll();
+
+    $response = $app->getResponse()->responseJson();
+
+    if (!count($menus)) {
+        $response
+            ->setStatus(Response::NO_CONTENT)
+            ->build();
+
+        return;
+    }
+
+    $menus = array_map(function ($menu) {
+        return $menu->toArray();
+    }, $menus);
+
+    $response
+        ->setStatus(Response::OK)
+        ->setBody($menus)
+        ->build();
 });
